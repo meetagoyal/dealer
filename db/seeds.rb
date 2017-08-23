0a9ec3b2383c3dc24e154f06de55c14c18d6a123
Meeta Goyal <meeta.goyal@mckesson.com>
2017-08-23 18:28:04 +0000

    more tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Car.delete_all
+Car.create(make: 'Toyota' , model: 'Camry', year: 2015, vin: '0123456789ASDFGHJ')
+Car.create(make: 'Honda' , model: 'Civic', year: 2015, vin: '0123456789ASDFGHH')
+Car.create(make: 'BMW' , model: 'X3', year: 2015, vin: '0123456789ASDFGHI')
